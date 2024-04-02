e9ab8e2d6ad687a87a0b2eab15cfe73dfaf9a5b2
Liisi Loomets <146342702+LLoomets@users.noreply.github.com>
2024-04-02 15:43:50 +0300

    Levelite eemaldamine selja taga

---
+++ Assets/Scripts/Enviroment/LevelDestroy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDestroy : MonoBehaviour
+{
+    public string parentName;
+
+    void Start()
+    {
+        parentName = transform.name;
+        StartCoroutine(DestroyClone());
+    }
+
+    IEnumerator DestroyClone()
+    {
+        yield return new WaitForSeconds(40);
+        if (parentName == "Section(Clone)")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
